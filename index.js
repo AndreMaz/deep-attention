a1b3b5c4d15f3f7e3708a569d9b295d1bd4967e8
AndreMaz <andre.maz.90@gmail.com>
2020-01-28 20:43:07 +0000

    Add model

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const tf = require("@tensorflow/tfjs-node");
 const { generateDataSet } = require("./dataset/generator");
+const { runSeq2SeqInference, createModel } = require("./models/lstm-attention");
+const dateFormat = require("./dataset/date_format");
 
 const configs = {
   dataset: {
@@ -9,11 +11,38 @@ const configs = {
     trainSplit: 0.25,
     /** Percentage of data used for validation */
     valSplit: 0.15
+  },
+  args: {
+    epochs: 2,
+    batchSize: 128
   }
 };
 
-function main() {
-  generateDataSet(configs.dataset);
+async function main() {
+  const {
+    trainEncoderInput,
+    trainDecoderInput,
+    trainDecoderOutput,
+    valEncoderInput,
+    valDecoderInput,
+    valDecoderOutput,
+    testDateTuples
+  } = generateDataSet(configs.dataset);
+
+  const model = createModel(
+    dateFormat.INPUT_VOCAB.length,
+    dateFormat.OUTPUT_VOCAB.length,
+    dateFormat.INPUT_LENGTH,
+    dateFormat.OUTPUT_LENGTH
+  );
+  model.summary();
+
+  await model.fit([trainEncoderInput, trainDecoderInput], trainDecoderOutput, {
+    epochs: configs.args.epochs,
+    batchSize: configs.args.batchSize,
+    shuffle: true,
+    validationData: [[valEncoderInput, valDecoderInput], valDecoderOutput]
+  });
 }
 
 main();
