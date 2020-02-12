c085756302d27af91187979988b35bb064d7604b
AndreMaz <andre.maz.90@gmail.com>
2020-02-12 15:27:42 +0000

    [WIP]

--- models/bahdanau-attention/index.js
+++ models/bahdanau-attention/index.js
@@ -4,8 +4,31 @@
  * More info: https://medium.com/datadriveninvestor/attention-in-rnns-321fbcd64f05
  */
 const tf = require("@tensorflow/tfjs");
-const dateFormat = require("../../dataset/date_format");
 
+/**
+ * @typedef {import('@tensorflow/tfjs').Tensor} Tensor
+ */
+
+const GetLastTimestepLayer = require("./last-time-step-layer");
+tf.serialization.registerClass(GetLastTimestepLayer);
+
+/**
+ * Create an LSTM-based attention model for date conversion.
+ *
+ * @param {number} inputVocabSize Input vocabulary size. This includes
+ *   the padding symbol. In the context of this model, "vocabulary" means
+ *   the set of all unique characters that might appear in the input date
+ *   string.
+ * @param {number} outputVocabSize Output vocabulary size. This includes
+ *   the padding and starting symbols. In the context of this model,
+ *   "vocabulary" means the set of all unique characters that might appear in
+ *   the output date string.
+ * @param {number} inputLength Maximum input length (# of characters). Input
+ *   sequences shorter than the length must be padded at the end.
+ * @param {number} outputLength Output length (# of characters).
+ * @param {string} alignmentType Type of Luong's alignment score. Can be `dot`, `general` or `concat`.
+ * @return {tf.LayersModel} A compiled model instance.
+ */
 function createModel(
   inputVocabSize,
   outputVocabSize,
@@ -16,32 +39,40 @@ function createModel(
   const embeddingDims = 64;
   const lstmUnits = 64;
 
-  const encoderEmbeddingInput = tf.input({
-    shape: [inputLength],
-    name: "embeddingEncoderInput"
-  });
+  // Layers shared across one time step
+  let static = {
+    repeator: tf.layers.repeatVector({ n: inputVocabSize }),
+    concatenator: tf.layers.concatenate({ axis: -1 }),
+    densor1: tf.layers.dense(10, { activation: "tanh" }),
+    densor2: tf.layers.dense(1, { activation: "relu" }),
+    activator: tf.layers.activation(softmax, { name: "attention_weights" }), // We are using a custom softmax(axis = 1) loaded in this notebook
+    dotor: tf.layers.dot({ axes: 1 })
+  };
 
-  // Select the embedding vectors by providing the `encoderEmbeddingInput` with the indices
-  let encoderEmbeddingOutput = tf.layers
-    .embedding({
-      inputDim: inputVocabSize,
-      outputDim: embeddingDims,
-      inputLength: inputLength,
-      maskZero: true,
-      name: "encoderEmbedding"
-    })
-    .apply(encoderEmbeddingInput);
-
-  // Feed the selected embedding vectors to the LSTM
-  let encoderGRUOutput = tf.layers
-    .bidirectional({
-      layer: tf.layers.gru({
-        units: lstmUnits,
-        returnSequences: true,
-        name: "encoderLSTM"
-      }) // `returnSequences` returns all the states. Not only the last one
-    })
-    .apply(encoderEmbeddingOutput);
+  return model;
 }
 
-module.exports = { createModel };
+/**
+ * @param {Tensor} a Encoder's Hidden States
+ * @param {Tensor} s_prev Decoder's previous hidden state
+ * @param {Object} static Static layers
+ */
+function oneStepAttention(a, s_prev, static) {
+  s_prev = static.repeator.apply(s_prev);
+
+  let concat = static.repeator.concatenator.apply([a, s_prev]);
+
+  let e = static.densor1.apply(concat);
+
+  let energies = static.densor2.apply(e);
+
+  let alphas = static.activator.apply(energies);
+
+  let context = static.dotor.apply([alphas, a]);
+
+  return context;
+}
+
+module.exports = {
+  createModel
+};
